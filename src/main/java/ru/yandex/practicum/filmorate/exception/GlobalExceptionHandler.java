4ab67058a6068e6632e428c58a69c5f65fbb1c86
Dmitriy Prishchepa <dimaprishchepa@yandex.ru>
2025-07-03 09:48:05 +0300

    add HandleCommonException commented

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -56,5 +56,12 @@ public class GlobalExceptionHandler {
         log.error("Element not found");
         return new ErrorResponse(e.getMessage());
     }
+
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public ErrorResponse handleCommonException(Exception e) {
+//        log.error("Server error");
+//        return new ErrorResponse(e.getMessage());
+//    }
 }
 
