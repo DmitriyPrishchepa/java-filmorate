80ea44def8bd0005be8f958a170aabb2f4850ec4
Dmitriy Prishchepa <dimaprishchepa@yandex.ru>
2025-07-05 21:18:46 +0300

    fix likes finally

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -68,28 +68,16 @@ public class InMemoryFilmStorage implements FilmStorage {
 
     @Override
     public void likeFilm(Long id, Long userId) {
-
         Film film = movies.get(id);
-
         Long filmLikes = film.getLikes();
-        Collection<Long> usersIdsLiked = film.getUsersIdsLiked();
-
-        if (!usersIdsLiked.contains(userId)) {
-            film.setLikes(filmLikes + 1);
-        }
+        film.setLikes(filmLikes + 1);
     }
 
     @Override
     public void unlikeFilm(Long id, Long userId) {
-
         Film film = movies.get(id);
-
         Long filmLikes = film.getLikes();
-        Collection<Long> usersIdsLiked = film.getUsersIdsLiked();
-
-        if (usersIdsLiked.contains(userId)) {
-            film.setLikes(filmLikes - 1);
-        }
+        film.setLikes(filmLikes - 1);
     }
 
     @Override
