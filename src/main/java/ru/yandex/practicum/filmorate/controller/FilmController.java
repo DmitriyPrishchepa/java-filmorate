6f2652982cb18fc8817c2728ed48b743872eba21
Dmitriy Prishchepa <dimaprishchepa@yandex.ru>
2025-07-07 13:31:11 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/films")
