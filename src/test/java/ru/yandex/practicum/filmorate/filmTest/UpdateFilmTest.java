eb177bc79a3e8e15317a72c7a853805edcf5e870
Dmitriy Prishchepa <dimaprishchepa@yandex.ru>
2025-09-09 09:46:54 +0300

    fix checkstyle and try to fix update film

--- src/test/java/ru/yandex/practicum/filmorate/filmTest/UpdateFilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/filmTest/UpdateFilmTest.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.model.Mpa;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
